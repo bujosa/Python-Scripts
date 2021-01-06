ce695c48153f04a2dca19493669f0a8753ade107
bujosa <davidbujosa@gmail.com>
2021-01-06 07:14:47 -0400

    Changed file name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import xlsxwriter
 workbook = xlsxwriter.Workbook('output.xlsx')
 worksheet = workbook.add_worksheet()
 
-with open('supercarrosdata2.csv') as csv_file:
+with open('supercarrosdata.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
