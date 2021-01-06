74b089eafb1fad9dc22c2f59ae4b08bebdb6e45e
bujosa <davidbujosa@gmail.com>
2021-01-06 08:06:42 -0400

    Added top car viewers

--- main.py
+++ main.py
@@ -4,19 +4,40 @@ import xlsxwriter
 workbook = xlsxwriter.Workbook('output.xlsx')
 worksheet = workbook.add_worksheet()
 
+workbook_two = xlsxwriter.Workbook('output2.xlsx')
+worksheet_two = workbook_two.add_worksheet()
+
 with open('supercarrosdata.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    line_count_two = 0
 
     top_car = {}
+    top_car_viewers = {}
+
     # first part "sacar top de vehiculos en base a cantidad de super carros"
     for row in csv_reader:
-        top_car[row] += 1
-        
+        if top_car.get(row[2]) == None:
+            top_car[row[2]] = 1
+        else:
+            top_car[row[2]] +=1                         
+
     for key in top_car:   
-        print(row)
         worksheet.write(line_count,0, key)
         worksheet.write(line_count,1, top_car[key])
         line_count+=1
+    
+    # second part "sacar top de vehiculos en base a cantidad de visitas"
+    for row in csv_reader:
+        if top_car_viewers.get(row[2]) == None:
+            top_car_viewers[row[2]] = row[19]
+        else:
+            top_car_viewers[row[2]] += row[19]                         
 
+    for key in top_car_viewers:   
+        worksheet_two.write(line_count_two,0, key)
+        worksheet_two.write(line_count_two,1, top_car_viewers[key])
+        line_count_two+=1
+
+workbook_two.close()
 workbook.close()
