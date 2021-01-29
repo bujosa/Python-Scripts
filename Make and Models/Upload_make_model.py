c7ba9918ba9c4c12c336293355f9842d84ed26d8
bujosa <davidbujosa@gmail.com>
2021-01-29 16:58:03 -0400

    Added new mutations

--- Make and Models/Upload_make_model.py
+++ Make and Models/Upload_make_model.py
@@ -5,10 +5,15 @@ import datetime
 import random
 import csv
 
+#Slug generator
+def slugGenerator(data):
+    data = data.lower().replace(" ","-")
+    return data
+
 for number in range(1992,2022):
   with open('${number}.csv') as csv_file:
       csv_reader = csv.reader(csv_file, delimiter=',')
-      line_count = 0
+      line_count = 0  
       line_count_two = 0
 
       top_car = {}
@@ -21,6 +26,8 @@ for number in range(1992,2022):
               url_car[row[2]] = row[18] 
           else:
               top_car[row[2]] +=1                         
+
+# SECOND PART
 
 # api_token = ""
 # headers = {"Authorization": "Bearer {token}".format(token=api_token)}
@@ -36,61 +43,60 @@ client = Client(
     fetch_schema_from_transport=True,
 )
 
-getModels = gql("""
-query{
-  getAllModels{
-    id
-    name
-    slug
-    brand{
+getMakes = gql("""
+  query{
+    makes{
       id
       name
-      slug
     }
   }
-}
+""")
+
+getModels = gql("""
+  query{
+      models{
+      id
+      name
+      make{
+        id
+        name
+      }
+    }
+  }
+""")
+
+createMakeMutation = gql("""
+  mutation($data: createMakeInput!){
+    createMake(input: $data){
+      make{
+        name
+      }
+    }
+  }
 """)
 
 createModelMutation = gql("""
-mutation($data:CreateCarInput!){
-  createCar(input:$data){
-    id
+  mutation($data: createModelInput!){
+    createModel(input: $data){
+      model{
+        id
+        name
+      }
+    }
   }
-}
 """)
 
 models = client.execute(
-    getModels)["getAllModels"]
+    getModels)["models"]
+
+makes = client.execute(getMakes)["makes"]
 
 
 for i in range(1):
-
-    priceIdx = random.randint(0, priceMaxIndex)
-    cvIdx = random.randint(0, contryVersionMaxIndex)
-    categoryIdx = random.randint(0, categoryMaxIndex)
-    dealerIdx = random.randint(0, dealerMaxIndex)
-    colorIdx = random.randint(0, colorMaxIndex)
-    bodyStyleIdx = random.randint(0, bodyStyleMaxIndex)
-    driveTrainIdx = random.randint(0, driveTrainMaxIndex)
-    modelIdx = random.randint(0, modelMaxIndex)
-    featureIdx = random.randint(0, featureMaxIndex)
-    fuelTypeIdx = random.randint(0, fuelTypeMaxIndex)
-
-    price = prices[priceIdx]
-    cv = countryVersions[cvIdx]
-    category = categories[categoryIdx]
-    dealer = dealers[dealerIdx]
-    color = colors[colorIdx]
-    bodyStyle = bodyStyles[bodyStyleIdx]
-    driveTrain = driveTrains[driveTrainIdx]
-    model = models[modelIdx]
-    feature = features[featureIdx]
-    fuelType = fuelTypes[fuelTypeIdx]
-
     modelParams = {
         "data": {
-            "name": "",
-            "slug": "pruebas",
+            "name": model,
+            "slug": slugGenerator(model),
             "visible": False,
         }
     }
