05260083986faba24a844cd635ea7891583c8d93
bujosa <davidbujosa@gmail.com>
2021-01-29 16:23:40 -0400

    Added new script

--- CsvFiles/main.py
+++ CsvFiles/main.py
@@ -14,17 +14,20 @@ with open('supercarrosdata_v2.csv') as csv_file:
 
     top_car = {}
     top_car_viewers = {}
+    url_car = {}
 
     # first part "sacar top de vehiculos en base a cantidad de super carros"
     for row in csv_reader:
         if top_car.get(row[2]) == None:
             top_car[row[2]] = 1
+            url_car[row[2]] = row[18] 
         else:
             top_car[row[2]] +=1                         
 
     for key in top_car:   
         worksheet.write(line_count,0, key)
         worksheet.write(line_count,1, top_car[key])
+        worksheet.write(line_count,2,url_car[key] )
         line_count+=1
     
     workbook.close()
@@ -35,12 +38,14 @@ with open('supercarrosdata_v2.csv') as csv_file:
         
     #     if top_car_viewers.get(row[2]) == None:
     #         top_car_viewers[row[2]] = int(row[19])
+    #         url_car[row[2]] = row[18]
     #     else:
     #         top_car_viewers[row[2]] += int(row[19])                         
 
     # for key in top_car_viewers:   
     #     worksheet_two.write(line_count_two,0, key)
     #     worksheet_two.write(line_count_two,1, top_car_viewers[key])
+    #     worksheet_two.write(line_count_two,2, url_car[key])
     #     line_count_two+=1
     
     # workbook_two.close()
